90a19224cae5f899dd68d637dc9605142038999e
IceQ1337 <IceQ1337@gmx.de>
2020-04-05 00:32:55 +0200

    fixed isValidSteamID()

--- utility.js
+++ utility.js
@@ -68,7 +68,7 @@ module.exports = function() {
                     reject(err);
                 });
             } else {
-                resolve();
+                reject();
             }
         });
     };
